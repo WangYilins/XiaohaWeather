1be8b4b4092e2b81142edc90dde1d8499d55379c
WangYilin <wangyilinv@gmail.com>
2019-06-11 21:47:51 +0800

    修改程序图标和名称

--- app/src/main/java/com/example/xiaohaweather/WeatherActivity.java
+++ app/src/main/java/com/example/xiaohaweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.xiaohaweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.xiaohaweather.gson.Forecast;
 import com.example.xiaohaweather.gson.Weather;
+import com.example.xiaohaweather.service.AutoUpdateService;
 import com.example.xiaohaweather.util.HttpUtil;
 import com.example.xiaohaweather.util.Utility;
 
@@ -185,6 +187,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic() {
